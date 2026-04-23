ccdab5228f4d9312e5e9a0e3fe69ec30d7448770
Mollie <imaobongdominic16@gmail.com>
2026-04-23 07:43:01 +0100

    Refactor SolanaProviders to use type-casted components

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,20 +7,23 @@ import {
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
 import { SOLANA_RPC_ENDPOINT } from '@/lib/constants';
-
-// Import wallet adapter CSS
 import '@solana/wallet-adapter-react-ui/styles.css';
+
+// Cast to any to fix React 18 + wallet adapter type conflict
+const CP  = ConnectionProvider  as any;
+const WP  = WalletProvider      as any;
+const WMP = WalletModalProvider as any;
 
 export function SolanaProviders({ children }: { children: React.ReactNode }) {
   const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
   return (
-    <ConnectionProvider endpoint={SOLANA_RPC_ENDPOINT}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
+    <CP endpoint={SOLANA_RPC_ENDPOINT}>
+      <WP wallets={wallets} autoConnect>
+        <WMP>
           {children}
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+        </WMP>
+      </WP>
+    </CP>
   );
 }
